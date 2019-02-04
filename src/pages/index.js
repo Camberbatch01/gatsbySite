8fc9ba755ed6c2207bd31ff021959639ff4a6dc7
camberbatch <42542153+Camberbatch01@users.noreply.github.com>
2019-02-04 15:46:01 +0000

    content change

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hello World!</h1>
+    <h1>Hello World!!</h1>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
