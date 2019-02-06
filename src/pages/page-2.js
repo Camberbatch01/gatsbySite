fa245975d708c2bf7a7223a419eadf15dba8de67
camberbatch <42542153+Camberbatch01@users.noreply.github.com>
2019-02-06 13:33:45 +0000

    added blog data to the page

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -4,16 +4,31 @@ import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+class BlogPage extends React.Component {
+  render(){
+    const data = this.props.data;
+    const posts = data.allMarkdownRemark.edges;
 
-
-const BlogPage = () => (
-  <Layout>
-    <SEO title="Page two" />
-    <h1>Blog</h1>
-    <p>Welcome to the blog</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+    return (
+      <Layout>
+          <SEO title="Page two" />
+          <h1>Blog</h1>
+          <p>Welcome to the blog</p>
+          {posts.map(post => {
+            //console.log(post);
+            return (
+              <div key={post.node.frontmatter.title}>
+                <h1>{post.node.frontmatter.title}</h1>
+                <small>{post.node.frontmatter.date}</small>
+                <p>{post.node.excerpt}</p>
+              </div>
+            );
+          })}
+          <Link to="/">Go back to the homepage</Link>
+        </Layout>
+    );
+  }
+}
 
 export default BlogPage
 
