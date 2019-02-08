e55c9ae1b1ec977106376549c710591b17b94f16
camberbatch <42542153+Camberbatch01@users.noreply.github.com>
2019-02-08 14:22:47 +0000

    bug fix attempt

--- src/pages/template/blogPost.js
+++ src/pages/template/blogPost.js
@@ -5,15 +5,18 @@ import Layout from "../../components/layout"
 import SEO from "../../components/seo"
 
 class BlogPost extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {data: this.props.location.state.postData.node}
+    }
     render(){
-        const data = this.props.location.state.postData.node;
         return (
             <Layout>
                 <SEO title="blog-post page" />
                 <div>
-                    <h1>{data.frontmatter.title}</h1>
-                    <small>{data.frontmatter.date}</small>
-                    <div>{data.html}</div>
+                    <h1>{this.state.data.frontmatter.title}</h1>
+                    <small>{this.state.data.frontmatter.date}</small>
+                    <div>{this.state.data.html}</div>
                 </div> 
             </Layout>    
         )
