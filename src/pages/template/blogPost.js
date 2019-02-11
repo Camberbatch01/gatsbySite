ff8ec23869536f84fd3b9da3e82596bb65cc338e
camberbatch <42542153+Camberbatch01@users.noreply.github.com>
2019-02-11 11:05:10 +0000

    pass specific blog data through and show it

--- src/pages/template/blogPost.js
+++ src/pages/template/blogPost.js
@@ -5,18 +5,31 @@ import Layout from "../../components/layout"
 import SEO from "../../components/seo"
 
 class BlogPost extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {data: this.props.location.state.postData.node}
+    constructor(){
+        super();
+        this.state = {
+            title: "",
+            date: "",
+            content: ""
+        }
+    }
+    componentDidMount(){
+        const pData = this.props.location.state.postData.node;
+
+        this.setState({
+            title: pData.frontmatter.title,
+            date: pData.frontmatter.date,
+            content: pData.html
+        });
     }
     render(){
         return (
-            <Layout>
+            <Layout location={this.props.location}>
                 <SEO title="blog-post page" />
                 <div>
-                    <h1>{this.state.data.frontmatter.title}</h1>
-                    <small>{this.state.data.frontmatter.date}</small>
-                    <div>{this.state.data.html}</div>
+                    <h1>{this.state.title}</h1>
+                    <small>{this.state.date}</small>
+                    <div>{this.state.content}</div>
                 </div> 
             </Layout>    
         )
