dbbd663681faaa6ea5b4bba4d5d5269fe3764cc3
camberbatch <42542153+Camberbatch01@users.noreply.github.com>
2019-02-15 13:18:39 +0000

    added twitter icon w/font awesome packages

--- src/components/header.js
+++ src/components/header.js
@@ -3,6 +3,12 @@ import PropTypes from "prop-types"
 import React from "react"
 import "../components/styles/nav.scss"
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+
+library.add(faTwitter)
+
 const Header = ({ siteTitle }) => (
   <header className="navBar">
     <ul className="container">
@@ -14,7 +20,7 @@ const Header = ({ siteTitle }) => (
       <li><Link className="navLinks" activeStyle={{ borderBottom: '2px solid white' }} to="/">Home</Link></li>
       <li><Link className="navLinks" activeStyle={{ borderBottom: '2px solid white' }} to="/page-2/">Blog</Link></li>
       <li><Link className="navLinks" activeStyle={{ borderBottom: '2px solid white' }} to="/contact">Contact</Link></li>
-      <li><Link className="navLinks" activeStyle={{ borderBottom: '2px solid white' }} to="/contact">t</Link></li>
+      <li><Link className="navLinks" activeStyle={{ borderBottom: '2px solid white' }} to="/contact"><FontAwesomeIcon icon={['fab', 'twitter']}/></Link></li>
     </ul>
   </header>
 )
