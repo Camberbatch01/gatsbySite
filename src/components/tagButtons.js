e7af9a83a1d86cf7abb40324cc3a64e78d848bd6
camberbatch <42542153+Camberbatch01@users.noreply.github.com>
2019-02-20 10:36:25 +0000

    tags positioned and styled on posts

--- src/components/tagButtons.js
+++ src/components/tagButtons.js
@@ -1,7 +1,7 @@
 import React from "react"
 import {Link} from "gatsby"
 
-const tagButtons = (tags, entries) =>  tags.map(tag => <Link to={`/page-2/?entries=${entries}&tag=${tag}`}>{tag}</Link>)
+const tagButtons = (tags, entries) =>  tags.map(tag => <Link className="tags" to={`/page-2/?entries=${entries}&tag=${tag}`}><label>{tag}</label></Link>)
 
 
 export default tagButtons
