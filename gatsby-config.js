b29a3369cec596e4c075668f07d3762221088975
camberbatch <42542153+Camberbatch01@users.noreply.github.com>
2019-03-06 18:31:50 +0000

    title, desc change

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Title`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Name LTD`,
+    description: `Website starter, with blog integration.`,
+    author: `Cameron Moore`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
